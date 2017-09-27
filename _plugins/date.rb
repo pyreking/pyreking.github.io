4e7e3904e2b9969e5b4bc96df96a0a0a03a2a2ee
Austin Guiney <austin.guiney@gmail.com>
2017-09-27 16:42:27 -0400

    Modified date.rb for more options, and improved the month layout prototype.

--- _plugins/date.rb
+++ _plugins/date.rb
@@ -3,11 +3,14 @@ require 'facets/integer/ordinal'
 
 module Jekyll
   module DateFilter
-    def pretty(date)
+    def ordinal(date)
       "#{date.strftime('%B')} #{date.strftime('%e').to_i.ordinalize}, #{date.strftime('%Y')}"
     end 
-    def shortpretty(date)
+    def shortordinal(date)
      "#{date.strftime('%b')} #{date.strftime('%e').to_i.ordinalize}, #{date.strftime('%Y')}"
+     end
+     def monthordinal(date)
+     "#{date.strftime('%B')} #{date.strftime('%e').to_i.ordinalize}"
      end
   end
 end
