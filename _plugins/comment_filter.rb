a997b5cb30912a6e413c6e5a5d35411338845d1f
Austin Guiney <austin.guiney@gmail.com>
2017-10-25 00:33:29 -0400

    Added a word filter for comments.

--- _plugins/comment_filter.rb
+++ _plugins/comment_filter.rb
@@ -1,9 +1,14 @@
 require 'sanitize'
+require 'obscenity'
 
 module Jekyll
   module CommentFilter
     def whitelist(html)
     Sanitize.fragment(html, elements:['strong','em', 'del', 'blockquote', 'code', "ul", 'li'])
+    end
+    def censor(html)
+    Obscenity.config.whitelist = ["genocide", "ass", "nipple", "aids", "crap", "as$", "suicide", "pimp"]
+    Obscenity.replacement(:stars).sanitize(html)
     end
   end
 end
