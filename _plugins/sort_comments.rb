be12712b5a43f5d0424f5539d70a48c2f3fed9fb
Austin Guiney <austin.guiney@gmail.com>
2017-11-01 11:19:39 -0400

    Added a sort_children method for threaded comments.

--- _plugins/sort_comments.rb
+++ _plugins/sort_comments.rb
@@ -1,17 +1,22 @@
 # Copyright (c) 2017 Austin Guiney.
 # MIT Licensed
+require 'pp'
 
 module Jekyll
   module SortComments
     def sort_comments(comments, attribute)
       hashes = to_array_of_hashes(comments)
-      hashes.sort_by!{|h| h[attribute]}
+      hashes.sort_by!{ |h| h[attribute]}
       return hashes
     end
     
-    def sort_comments_by_depth(comments)
-    
-    
+    def sort_children(comments)
+    	nested_hash = Hash[comments.map{|e| [e["_id"], e.merge(children: [])]}]
+		nested_hash.each do |id, item|
+ 			parent = nested_hash[item["parent"]]
+  			parent[:children] << item if parent
+		end
+		tree = nested_hash.select { |id, item| item["parent"] == "" }.values
     end
     
     def to_array_of_hashes(comments) 
