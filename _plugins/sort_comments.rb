a5e47126472e80b774c6722ecc23cfd38342b30a
Austin Guiney <austin.guiney@gmail.com>
2017-11-02 21:14:15 -0400

    Finished a simplified version of the sort_children method.

--- _plugins/sort_comments.rb
+++ _plugins/sort_comments.rb
@@ -1,26 +1,40 @@
 # Copyright (c) 2017 Austin Guiney.
 # MIT Licensed
 require 'pp'
+require 'rails'
+require 'sparsify'
 
 module Jekyll
   module SortComments
+  
+  	# Sorts comments by a user-specified attribute
     def sort_comments(comments, attribute)
       hashes = to_array_of_hashes(comments)
       hashes.sort_by!{ |h| h[attribute]}
       return hashes
     end
     
+    # Returns a tree-based data structure for comments
     def sort_children(comments)
-    	nested_hash = Hash[comments.map{|e| [e["_id"], e.merge(children: [])]}]
-		nested_hash.each do |id, item|
- 			parent = nested_hash[item["parent"]]
-  			parent[:children] << item if parent
-		end
-		tree = nested_hash.select { |id, item| item["parent"] == "" }.values
+    	sorted = []
+    	children = comments.select{|k,v| k["parent"] != ""}
+    	
+    	for i in comments
+    		if i["parent"] == ""
+    			sorted << i
+    		end
+    		for j in children
+    			if i["_id"] == j["parent"]
+    				sorted << j
+    			end
+    		end
+    	end
+    	return sorted
     end
     
-    def to_array_of_hashes(comments) 
-      hash = comments.sort.to_h
+    # Turns an array of strings into an array of hashes
+    def to_array_of_hashes(string) 
+      hash = string.sort.to_h
       array_of_hashes = []
       hash.values.each_with_index do |item, index|
         array_of_hashes[index] = item.to_h
@@ -29,5 +43,4 @@ module Jekyll
 	end
   end
 end
-    
 Liquid::Template.register_filter(Jekyll::SortComments)
