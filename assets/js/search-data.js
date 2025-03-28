38e41ff86bdf1732d356738398cf7c70bb9ff280
pyreking <austin.guiney@gmail.com>
2025-03-28 02:04:33 +0000

    Deploying to gh-pages from @ pyreking/pyreking.github.io@d00e1d4b18629ece23919a62d0aca5ec1b93e0ad 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          description: "courses and supplemental instruction",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
